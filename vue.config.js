cd2a2a2759b63b896bf8c0ab9951fbfa8863b036
Yeonwoo Kim <wiskingdom@gmail.com>
2021-01-06 20:52:22 +0900

    Init

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+var fs = require("fs");
+var path = require("path");
+var homedir = require("os").homedir();
+
+module.exports = {
+  devServer: {
+    port: 3000,
+    https: true,
+    key: fs.readFileSync(
+      path.resolve(`${homedir}/.office-addin-dev-certs/localhost.key`)
+    ),
+    cert: fs.readFileSync(
+      path.resolve(`${homedir}/.office-addin-dev-certs/localhost.crt`)
+    ),
+    ca: fs.readFileSync(
+      path.resolve(`${homedir}/.office-addin-dev-certs/ca.crt`)
+    )
+  }
+};
